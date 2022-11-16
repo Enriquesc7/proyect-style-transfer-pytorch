86ebdb300c84e791b51211c369906f3038c951a0
Enrique <enrique.sc79@gmail.com>
2022-11-16 17:06:25 -0300

    Cambiando lr=0.2

--- style_transfer/style_transfer.py
+++ style_transfer/style_transfer.py
@@ -299,7 +299,7 @@ class StyleTransfer:
                 end_scale: int = 512,
                 iterations: int = 500,
                 initial_iterations: int = 1000,
-                step_size: float = 0.002,
+                step_size: float = 0.2,
                 avg_decay: float = 0.99,
                 init: str = 'content',
                 style_scale_fac: float = 1.,
